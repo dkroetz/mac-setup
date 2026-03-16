73287aa265f72c9dadb9f2b1b08f3ca5275897a8
dkroetz <denis.kroetz@gmail.com>
2026-03-16 09:31:17 +0100

    chore(editor): refresh local editor configs
    Sync shell helpers, lazygit and Zed settings for consistent tooling defaults.

--- fish/config.fish
+++ fish/config.fish
@@ -5,11 +5,19 @@ if status is-interactive
 end
 
 # Global aliases
+abbr --add oc opencode
 abbr --add vim nvim
 abbr --add lg lazygit
 abbr --add python python3
 abbr --add clip pbcopy
+abbr --add cat "bat -p"
 
 # Global env vars
 ## Opencode
 set -gx OPENCODE_ENABLE_EXA 1
+
+## Lazygit config dir
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+
+## zoxide conf
+set -gx _ZO_EXCLUDE_DIRS "/tmp:/var:/proc:/sys:/.venv:/node_modules:/__pycache__"
